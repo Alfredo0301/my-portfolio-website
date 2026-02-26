c6092d9db3256faf73b08b546f329cf1bce44442
Alfredo <alfrdons.jr@gmail.com>
2026-02-26 14:46:36 -0300

    feat: initial project structure with Spring Boot

---
+++ src/test/java/com/portfolio/PortfolioWebsite/PortfolioWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PortfolioWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
