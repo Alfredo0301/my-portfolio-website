afa988052c463a966d0bb5e93931631882f4720b
Alfredo <alfrdons.jr@gmail.com>
2026-03-22 22:19:40 -0300

    feat: initial project structure with Spring Boot

--- src/main/java/com/portfolio/PortfolioWebsite/PortfolioWebsiteApplication.java
+++ src/main/java/com/portfolio/PortfolioWebsite/PortfolioWebsiteApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioWebsiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioWebsiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioWebsiteApplication.class, args);
+    }
 
 }
